d958096dc64203eefc1d826d9bdf3e23e8802d3d
lkimasu <lwlstjdl@naver.com>
2023-12-07 17:39:49 +0900

    update db info

--- src/db.php
+++ src/db.php
@@ -1,10 +1,11 @@
 <?php
     
-	header('Content-Type: text/html; charset=utf-8'); // utf-8인코딩
-
-	// localhost = DB주소, web=DB계정아이디, 1234=DB계정비밀번호, post_board="DB이름"
-	$db = new mysqli("localhost","myuser","mypassword","mydatabases"); 
+	$db = new mysqli("db","devuser","devpass","test_db"); 
 	$db->set_charset("utf8");
+
+	if($db -> connect_error){
+		echo 'connection failed' . $db -> connect_error;
+	}
 
 	function mq($sql)
 	{
